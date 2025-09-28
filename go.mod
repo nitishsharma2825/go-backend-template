862a610bbecd61d8f7cd1bd09dd30d3fe03fb6f1
nitishsharma2825 <sharnit2825@gmail.com>
2025-09-28 14:20:51 +0530

    replaced http library to chi + hot reloading using air-verse

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nitishsharma2825/social
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
--- src/api/app.go
+++ src/api/app.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -13,15 +16,32 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
 
-	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	// Add some basic middleware to the router
+	r.Use(middleware.RequestID) // Assigns a request ID to each request
+	r.Use(middleware.RealIP)    // Sets the RemoteAddr to the value of X-Real-IP or X-Forwarded-For
+	r.Use(middleware.Logger)    // Logs the start and end of each request with the elapsed processing time
+	r.Use(middleware.Recoverer) // Recover from panics without crashing server
 
-	return mux
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(mux *chi.Mux) error {
 	server := &http.Server{
 		Addr:         app.config.addr,
 		Handler:      mux,
